b816de7771ff87f8bdfa4444408b48743755cac3
bcassell <bcassell@umich.edu>
2013-06-30 16:32:22 -0400

    observation validator

---
+++ lib/backend/observation_validator.rb
@@ -0,0 +1,67 @@
+require 'multi_json'
+require_relative '../util/object_numerism'
+
+class ObservationValidator
+  def self.validate(profile, file)
+    begin
+      json = File.open(file).read
+      data_hash = MultiJson.load(json)
+      if matches_profile?(profile, data_hash) && payoffs_are_valid?(data_hash)
+        filter_content(data_hash)
+      end
+    rescue Exception => e
+      puts e.message
+    end
+  end
+
+  private
+
+  def self.convert_payoffs(data)
+
+  end
+
+  def self.filter_content(data)
+    data["symmetry_groups"] = data["symmetry_groups"].map do |sgroup|
+      sgroup["players"] = sgroup["players"].map do |player|
+        player["payoff"] = Float(player["payoff"])
+        player.keep_if { |k,v| ["payoff","features"].include?(k) }
+      end
+      sgroup.keep_if { |k,v| ["role","strategy","players"].include?(k) }
+    end
+    data.keep_if { |k,v| ["features","symmetry_groups"].include?(k) }
+  end
+
+  def self.payoffs_are_valid?(data)
+    data["symmetry_groups"].detect do |symmetry_group|
+      symmetry_group["players"].detect do |player|
+        payoff_invalid?(player)
+      end
+    end == nil
+  end
+
+  def self.payoff_invalid?(player)
+    !player['payoff'].numeric?
+  end
+
+  def self.matches_profile?(profile, data)
+    data = data["symmetry_groups"].map{ |sgroup| signature_of_hash(sgroup) }
+    pdata = profile.symmetry_groups.map do |sgroup|
+      signature_of_symmetry_group(sgroup)
+    end
+    data = data.sort{ |x,y| x[:role]+x[:strategy] <=> y[:role]+y[:strategy] }
+    pdata = pdata.sort{ |x,y| x[:role]+x[:strategy] <=> y[:role]+y[:strategy] }
+    data == pdata
+  end
+
+  def self.signature_of_hash(symmetry_group)
+    { role: symmetry_group["role"],
+      strategy: symmetry_group["strategy"],
+      count: symmetry_group["players"].size }
+  end
+
+  def self.signature_of_symmetry_group(symmetry_group)
+    { role: symmetry_group.role,
+      strategy: symmetry_group.strategy,
+      count: symmetry_group.count }
+  end
+end
---
+++ spec/lib/backend/observation_validator_spec.rb
@@ -0,0 +1,380 @@
+require 'backend/observation_validator'
+
+describe ObservationValidator do
+  describe 'validate' do
+    let(:symmetry_groups) do
+      [double(role: 'Buyer', strategy: 'BidValue', count: 2),
+       double(role: 'Seller', strategy: 'Shade1', count: 1),
+       double(role: 'Seller', strategy: 'Shade2', count: 1)]
+    end
+    let(:profile){ double(symmetry_groups: symmetry_groups) }
+    let(:path){ 'fake/path' }
+
+    before do
+      file = double(read: file_contents)
+      File.should_receive(:open).with(path).and_return(file)
+    end
+
+    context 'when everything is valid' do
+      let(:file_contents) do
+        <<-JSON
+          {
+            "features": {
+              "featureA": 34.0,
+              "featureB": [37, 38],
+              "featureC": {
+                "C1": 40.0, "C2": 42.0
+              }
+            },
+            "symmetry_groups": [
+              {
+                "role": "Buyer",
+                "strategy": "BidValue",
+                "players": [
+                  {
+                    "payoff": 2992.73,
+                    "features": {
+          				    "featureA": 0.001,
+          				    "featureB": [2.0, 2.1]
+          			    }
+          			  },
+            			{
+            			  "payoff": 2990.53,
+              			"features": {
+              				"featureA": 0.002,
+              				"featureB": [2.0, 2.1]
+            			  }
+                  }
+                ]
+              },
+              {
+                "role": "Seller",
+                "strategy": "Shade1",
+                "players": [
+                  {
+                    "payoff": 2929.34,
+          			    "features": {
+          				    "featureA": 0.003,
+          				    "featureB": [1.3, 1.7]
+          			    }
+          			  }
+                ]
+              },
+              {
+                "role": "Seller",
+                "strategy": "Shade2",
+                "players": [
+          			  {
+          			    "payoff": 2924.44,
+          			    "features": {
+          				    "featureA": 0.003,
+          				    "featureB": [1.4, 1.7]
+          			    }
+          			  }
+                ]
+              }
+            ]
+          }
+        JSON
+      end
+      let(:outcome) do
+        {
+          "features" => {
+            "featureA" => 34.0,
+            "featureB" => [37, 38],
+            "featureC" => {
+              "C1" => 40.0, "C2" => 42.0
+            }
+          },
+          "symmetry_groups" => [
+            {
+              "role" => 'Buyer',
+              "strategy" => 'BidValue',
+              "players" => [
+                {
+                  "payoff" => 2992.73,
+        			    "features" => {
+        				    "featureA" => 0.001,
+        				    "featureB" => [2.0, 2.1]
+        			    }
+        			  },
+        			  {
+        			    "payoff" => 2990.53,
+          			  "features" => {
+          				  "featureA" => 0.002,
+          				  "featureB" => [2.0, 2.1]
+          			  }
+        			  }
+              ]
+            },
+            {
+              "role" => 'Seller',
+              "strategy" => 'Shade1',
+              "players" => [
+                "payoff" => 2929.34,
+        			  "features" => {
+        				  "featureA" => 0.003,
+        				  "featureB" => [1.3, 1.7]
+        			  }
+              ]
+            },
+            {
+              "role" => 'Seller',
+              "strategy" => 'Shade2',
+              "players" => [
+        			  "payoff" => 2924.44,
+        			  "features" => {
+        				  "featureA" => 0.003,
+        				  "featureB" => [1.4, 1.7]
+        			  }
+              ]
+            }
+          ]
+        }
+      end
+
+      it { ObservationValidator.validate(profile, path).should eql(outcome) }
+
+      context 'when the data does not match the profile' do
+        let(:symmetry_groups) do
+          [double(role: 'Buyer', strategy: 'BidValue', count: 2),
+           double(role: 'Seller', strategy: 'Shade2', count: 1),
+           double(role: 'Seller', strategy: 'Shade3', count: 1)]
+         end
+
+         it { ObservationValidator.validate(profile, path).should == nil }
+      end
+
+      context 'when there is extra content' do
+        let(:file_contents) do
+          <<-JSON
+            {
+              "extra": "content",
+              "features": {
+                "featureA": 34.0,
+                "featureB": [37, 38],
+                "featureC": {
+                  "C1": 40.0, "C2": 42.0
+                }
+              },
+              "symmetry_groups": [
+                {
+                  "role": "Buyer",
+                  "strategy": "BidValue",
+                  "count": 2,
+                  "players": [
+                    {
+                      "payoff": 2992.73,
+                      "features": {
+            				    "featureA": 0.001,
+            				    "featureB": [2.0, 2.1]
+            			    }
+            			  },
+              			{
+              			  "payoff": 2990.53,
+                			"features": {
+                				"featureA": 0.002,
+                				"featureB": [2.0, 2.1]
+              			  },
+              			  "other_extra": 23
+                    }
+                  ]
+                },
+                {
+                  "role": "Seller",
+                  "strategy": "Shade1",
+                  "players": [
+                    {
+                      "payoff": 2929.34,
+            			    "features": {
+            				    "featureA": 0.003,
+            				    "featureB": [1.3, 1.7]
+            			    }
+            			  }
+                  ]
+                },
+                {
+                  "role": "Seller",
+                  "strategy": "Shade2",
+                  "players": [
+            			  {
+            			    "payoff": 2924.44,
+            			    "features": {
+            				    "featureA": 0.003,
+            				    "featureB": [1.4, 1.7]
+            			    }
+            			  }
+                  ]
+                }
+              ]
+            }
+          JSON
+        end
+
+        it 'is filtered out' do
+          ObservationValidator.validate(profile, path).should eql(outcome)
+        end
+      end
+
+      context 'when there are string numeric payoff values' do
+        let(:file_contents) do
+          <<-JSON
+            {
+              "features": {
+                "featureA": 34.0,
+                "featureB": [37, 38],
+                "featureC": {
+                  "C1": 40.0, "C2": 42.0
+                }
+              },
+              "symmetry_groups": [
+                {
+                  "role": "Buyer",
+                  "strategy": "BidValue",
+                  "players": [
+                    {
+                      "payoff": "2992.73",
+                      "features": {
+            				    "featureA": 0.001,
+            				    "featureB": [2.0, 2.1]
+            			    }
+            			  },
+              			{
+              			  "payoff": "2990.53",
+                			"features": {
+                				"featureA": 0.002,
+                				"featureB": [2.0, 2.1]
+              			  }
+                    }
+                  ]
+                },
+                {
+                  "role": "Seller",
+                  "strategy": "Shade1",
+                  "players": [
+                    {
+                      "payoff": "2929.34",
+            			    "features": {
+            				    "featureA": 0.003,
+            				    "featureB": [1.3, 1.7]
+            			    }
+            			  }
+                  ]
+                },
+                {
+                  "role": "Seller",
+                  "strategy": "Shade2",
+                  "players": [
+            			  {
+            			    "payoff": "2924.44",
+            			    "features": {
+            				    "featureA": 0.003,
+            				    "featureB": [1.4, 1.7]
+            			    }
+            			  }
+                  ]
+                }
+              ]
+            }
+          JSON
+        end
+
+        it 'they get converted to floats' do
+          ObservationValidator.validate(profile, path).should eql(outcome)
+        end
+      end
+    end
+
+    context 'when there are non-numeric payoffs' do
+      let(:file_contents) do
+        <<-JSON
+          {
+            "symmetry_groups": [
+              {
+                "role": "Buyer",
+                "strategy": "BidValue",
+                "count": 2,
+                "players": [
+                  {
+                    "payoff": "FAIL"
+          			  },
+            			{
+            			  "payoff": 2990.53
+                  }
+                ]
+              },
+              {
+                "role": "Seller",
+                "strategy": "Shade1",
+                "count": 1,
+                "players": [
+                  {
+                    "payoff": 2929.34
+          			  }
+                ]
+              },
+              {
+                "role": "Seller",
+                "strategy": "Shade2",
+                "count": 1,
+                "players": [
+          			  {
+          			    "payoff": 2924.44
+          			  }
+                ]
+              }
+            ]
+          }
+        JSON
+      end
+
+      it { ObservationValidator.validate(profile, path).should == nil }
+    end
+
+    context 'when there are NaN or Inf payoffs' do
+      let(:file_contents) do
+        <<-JSON
+          {
+            "symmetry_groups": [
+              {
+                "role": "Buyer",
+                "strategy": "BidValue",
+                "count": 2,
+                "players": [
+                  {
+                    "payoff": "NaN"
+          			  },
+            			{
+            			  "payoff": 2990.53
+                  }
+                ]
+              },
+              {
+                "role": "Seller",
+                "strategy": "Shade1",
+                "count": 1,
+                "players": [
+                  {
+                    "payoff": 2929.34
+          			  }
+                ]
+              },
+              {
+                "role": "Seller",
+                "strategy": "Shade2",
+                "count": 1,
+                "players": [
+          			  {
+          			    "payoff": 2924.44
+          			  }
+                ]
+              }
+            ]
+          }
+        JSON
+      end
+
+      it { ObservationValidator.validate(profile, path).should == nil }
+    end
+  end
+end
