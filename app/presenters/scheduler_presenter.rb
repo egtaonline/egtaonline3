f36b6738eafa4356a0d33071b4e814363b9f369f
bcassell <bcassell@umich.edu>
2013-07-12 17:46:46 -0400

    scheduler presenter is basically working

--- app/presenters/scheduler_presenter.rb
+++ app/presenters/scheduler_presenter.rb
@@ -13,7 +13,30 @@ class SchedulerPresenter
   end
 
   def with_requirements
-
+    sql = <<-SQL
+      select row_to_json(t)
+      from (
+        select schedulers.id, name, type, active, process_memory,
+        time_per_observation, observations_per_simulation,
+        default_observation_requirement, nodes, size, simulator_id,
+        (
+          hstore_to_matrix(configuration)
+        ) as configuration,
+        (
+          select array_to_json(array_agg(scheduling_requirement))
+          from (
+            select profile_id, count as requirement,
+            observations_count as current_count
+            from profiles, scheduling_requirements
+            where profiles.id = scheduling_requirements.profile_id
+            and scheduling_requirements.scheduler_id = schedulers.id
+          ) scheduling_requirement
+        ) as scheduling_requirements
+        from schedulers, simulator_instances
+        where schedulers.id = #{@scheduler.id}
+        and schedulers.simulator_instance_id = simulator_instances.id
+      ) t
+    SQL
   end
 end
   # def json_specialization
---
+++ spec/presenters/scheduler_presenter_spec.rb
@@ -0,0 +1,68 @@
+require 'spec_helper'
+
+describe SchedulerPresenter do
+  describe '#to_json' do
+    context 'when no granularity is specified' do
+      it 'defaults to the normal to_json' do
+        scheduler = double('scheduler')
+        scheduler.should_receive(:to_json)
+        SchedulerPresenter.new(scheduler).to_json
+      end
+    end
+
+    context 'when the with_requirements granularity is given' do
+      let(:response) do
+        "{\"id\":#{scheduler.id},\"name\":\"#{scheduler.name}\",\"type\":"+
+        "\"#{scheduler.class}\",\"active\":#{scheduler.active}," +
+        "\"process_memory\":#{scheduler.process_memory}," +
+        "\"time_per_observation\":#{scheduler.time_per_observation}," +
+        "\"observations_per_simulation\":"+
+        "#{scheduler.observations_per_simulation}," +
+        "\"default_observation_requirement\":"+
+        "#{scheduler.default_observation_requirement}," +
+        "\"nodes\":#{scheduler.nodes},\"size\":#{scheduler.size},"+
+        "\"simulator_id\":#{scheduler.simulator_instance.simulator_id}," +
+        "\"configuration\":" +
+        scheduler.simulator_instance.configuration.collect do |k,v|
+            [k,v.to_s]
+        end.to_json +
+        ",\"scheduling_requirements\":" +
+        scheduler.scheduling_requirements.collect do |requirement|
+          { profile_id: requirement.profile_id, requirement: requirement.count,
+            current_count: requirement.observations_count }
+        end.to_json + "}"
+      end
+      context 'and the scheduler is non-generic' do
+        let(:scheduler){ FactoryGirl.create(:game_scheduler) }
+        before do
+          scheduler.simulator.add_strategy('All', 'A')
+          scheduler.simulator.add_strategy('All', 'B')
+          scheduler.add_role('All', 2)
+          scheduler.add_strategy('All', 'A')
+          scheduler.add_strategy('All', 'B')
+        end
+
+        it 'returns the scheduling requirements' do
+          SchedulerPresenter.new(scheduler).to_json(
+            granularity: "with_requirements").should == response
+        end
+      end
+      context 'and the scheduler is generic' do
+        let(:scheduler){ FactoryGirl.create(:generic_scheduler) }
+        before do
+          scheduler.simulator.add_strategy('All', 'A')
+          scheduler.simulator.add_strategy('All', 'B')
+          scheduler.add_role('All', 2)
+          scheduler.add_profile('All: 2 A', 10)
+          scheduler.add_profile('All: 1 A, 1 B', 15)
+          scheduler.add_profile('All: 2 B', 20)
+        end
+
+        it 'returns the scheduling requirements' do
+          SchedulerPresenter.new(scheduler).to_json(
+            granularity: "with_requirements").should == response
+        end
+      end
+    end
+  end
+end
