41af5dd856f9e379ef99320602beaf2f13bc6f22
bcassell <bcassell@umich.edu>
2013-08-14 11:14:11 -0400

    migration

---
+++ db/migrate/20130812174745_add_preaggregates.rb
@@ -0,0 +1,23 @@
+class AddPreaggregates < ActiveRecord::Migration
+  self.disable_ddl_transaction!
+  def up
+    add_column :symmetry_groups, :payoff, :float
+    add_column :symmetry_groups, :payoff_sd, :float
+    create_table :observation_aggs do |t|
+      t.integer :observation_id, :null => false
+      t.integer :symmetry_group_id, :null => false
+      t.float :payoff, :null => false
+      t.float :payoff_sd
+    end
+
+    execute "INSERT INTO observation_aggs(observation_id, symmetry_group_id, payoff, payoff_sd) SELECT observation_id, symmetry_group_id, avg(payoff), stddev_samp(payoff) FROM players GROUP BY observation_id, symmetry_group_id"
+    add_index :observation_aggs, [:observation_id, :symmetry_group_id], unique: true
+  end
+
+  def down
+    remove_index :observation_aggs, [:observation_id, :symmetry_group_id]
+    drop_table :observation_aggs
+    remove_column :symmetry_groups, :payoff_sd
+    remove_column :symmetry_groups, :payoff, :float
+  end
+end
