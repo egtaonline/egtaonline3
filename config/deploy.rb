fbb2a316c67abe219a59b83d1361ebf6c084c085
bcassell <bcassell@umich.edu>
2013-08-18 12:08:37 -0400

    update puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,13 @@
 require 'bundler/capistrano'
-# require 'puma/capistrano'
+require 'puma/capistrano'
 
 load 'config/recipes/base'
-# load 'config/recipes/foreman'
-# load 'config/recipes/nginx'
-# load 'config/recipes/nodejs'
+load 'config/recipes/foreman'
+load 'config/recipes/nginx'
+load 'config/recipes/nodejs'
 load 'config/recipes/postgresql'
-# load 'config/recipes/rbenv'
-# load 'config/recipes/redis'
+load 'config/recipes/rbenv'
+load 'config/recipes/redis'
 
 server 'egtaonline.eecs.umich.edu', :web, :app, :db, primary: true
 
