f54d5877344e422902c6331db1cd93b5110f2d7b
Yao <liuyaoly@umich.edu>
2014-08-12 19:09:36 -0400

    finish testing codes for scriptArgumentSetter case 1

--- spec/lib/analysis/scripts_argument_setter.rb
+++ spec/lib/analysis/scripts_argument_setter.rb
@@ -1,169 +1,245 @@
-# require 'analysis'
+require 'analysis'
 
-# describe ScriptsArgumentSetter do
-# 	let(:analysis_obj){double("analysis_obj")}
-# 	let(:reduction_obj){double ("reduction_obj")}
-# 	let(:subgame_obj){double("subgame_obj")}
-# 	let(:game){ double ("game")}
-# 	let(:path_obj){double("path object")}
-# 	context "when reduction and subgame options are checked" do
-# 		before(:each) do
-# 			@setter = ScriptsArgumentSetter.new(analysis_obj,reduction_obj,subgame_obj)				
-# 			@setter.set_path(path_obj)
-# 		end
+describe ScriptsArgumentSetter do
+	let(:analysis_obj){double("analysis_obj")}
+	let(:reduction_obj){double ("reduction_obj")}
+	let(:subgame_obj){double("subgame_obj")}
+	let(:game){ double ("game")}
+	let(:path_obj){double("path object")}
 
-# 		describe "#set_path" do
-# 			it "sets the right paht_finder object" do
-# 				@setter = ScriptsArgumentSetter.new(analysis_obj,reduction_obj,subgame_obj)				
-# 				@setter.set_path(path_obj).should == path_obj
-# 			end
-# 		end
 
-# 		describe "#prepare_input" do
-# 			local_input_path = "local/analysis/game-id/in"
-# 			input_file_name = "game-id-analysis-time.json"
-# 			local_subgame_path = "local/analysis/game-id/subgame"
-# 			subgame_json_file_name = "subgame.json"
+	context "when reduction and subgame options are checked" do
+		before(:each) do
+			@setter = ScriptsArgumentSetter.new(analysis_obj,reduction_obj,subgame_obj)				
+			@setter.set_path(path_obj)
+		end
 
-# 			it "prepares the analysis script input" do
+		describe "#set_path" do
+			it "sets the right paht_finder object" do
+				@setter = ScriptsArgumentSetter.new(analysis_obj,reduction_obj,subgame_obj)				
+				@setter.set_path(path_obj).should == path_obj
+			end
+		end
+
+		describe "#prepare_input" do
+			local_input_path = "local/analysis/game-id/in"
+			input_file_name = "game-id-analysis-time.json"
+			local_subgame_path = "local/analysis/game-id/subgame"
+			subgame_json_file_name = "subgame.json"
+
+			it "prepares the analysis script input" do
 				
-# 				path_obj.should_receive(:local_input_path).and_return(local_input_path)
-# 				path_obj.should_receive(:input_file_name).and_return(input_file_name)
-# 				analysis_obj.should_receive(:prepare_input).with(game,local_input_path,input_file_name)
+				path_obj.should_receive(:local_input_path).and_return(local_input_path)
+				path_obj.should_receive(:input_file_name).and_return(input_file_name)
+				analysis_obj.should_receive(:prepare_input).with(game,local_input_path,input_file_name)
 
-# 				path_obj.stub(:local_subgame_path).and_return(local_subgame_path)
-# 				path_obj.stub(:subgame_json_file_name).and_return(subgame_json_file_name)
-# 				subgame_obj.stub(:prepare_input).with(game,local_subgame_path, subgame_json_file_name)
-# 				@setter.prepare_input(game)
-# 			end
+				path_obj.stub(:local_subgame_path).and_return(local_subgame_path)
+				path_obj.stub(:subgame_json_file_name).and_return(subgame_json_file_name)
+				subgame_obj.stub(:prepare_input).with(game,local_subgame_path, subgame_json_file_name)
+				@setter.prepare_input(game)
+			end
 			
-# 			it "prepares the subgame script input" do
+			it "prepares the subgame script input" do
 				
-# 				path_obj.stub(:local_input_path).and_return(local_input_path)
-# 				path_obj.stub(:input_file_name).and_return(input_file_name)
-# 				analysis_obj.stub(:prepare_input).with(game,local_input_path,input_file_name)
+				path_obj.stub(:local_input_path).and_return(local_input_path)
+				path_obj.stub(:input_file_name).and_return(input_file_name)
+				analysis_obj.stub(:prepare_input).with(game,local_input_path,input_file_name)
 
-# 				path_obj.should_receive(:local_subgame_path).and_return(local_subgame_path)
-# 				path_obj.should_receive(:subgame_json_file_name).and_return(subgame_json_file_name)
-# 				subgame_obj.should_receive(:prepare_input).with(game,local_subgame_path, subgame_json_file_name)
+				path_obj.should_receive(:local_subgame_path).and_return(local_subgame_path)
+				path_obj.should_receive(:subgame_json_file_name).and_return(subgame_json_file_name)
+				subgame_obj.should_receive(:prepare_input).with(game,local_subgame_path, subgame_json_file_name)
 				
-# 				@setter.prepare_input(game)
-# 			end
+				@setter.prepare_input(game)
+			end
 
-# 		end
+		end
 
-# 		descrit "#set_up_remote_command" do
-# 			it "sets up the remote for each of the script" do
-# 				work_dir = "$JOB_ID"
-# 				remote_input_path = "remote_input_path"
-# 				input_file_name = "input_file_name"
-# 				reduction_script_path = "reduction_script_path"
-# 				remote_subgame_path = "remote_subgame_path"
-# 				subgame_json_file_name = "subgame_json_file_name"
-# 				subgame_script_path = "subgame_script_path"
-# 				analysis_script_path = "analysis_script_path"
+		describe "#set_up_remote_command" do
+			it "sets up the remote for each of the script" do
 
-# 				analysis_set_up_remote_script_command = "analysis_set_up_remote_script_command"
-# 				analysis_set_up_remote_input_command = "analysis_set_up_remote_input_command"
-# 				reduction_set_up_command = "reduction_set_up_command"
-# 				subgame_set_up_command = "subgame_set_up_command"
+				work_dir = "$JOB_ID"
+				remote_input_path = "remote_input_path"
+				input_file_name = "input_file_name"
+				reduction_script_path = "reduction_script_path"
+				remote_subgame_path = "remote_subgame_path"
+				subgame_json_file_name = "subgame_json_file_name"
+				subgame_script_path = "subgame_script_path"
+				analysis_script_path = "analysis_script_path"
+				scripts_path = "scripts_path"
 
-# 				path_obj.stub(:analysis_script_path).and_return(analysis_script_path)
-# 				path_obj.stub(:remote_input_path).and_return(remote_input_path)
-# 				path_obj.stub(:input_file_name).and_return(input_file_name)
-# 				path_obj.stub(:reduction_script_path).and_return(reduction_script_path)
-# 				path_obj.stub
+				analysis_set_up_remote_script_command = "analysis_set_up_remote_script_command"
+				analysis_set_up_remote_input_command = "analysis_set_up_remote_input_command"
+				reduction_set_up_command = "reduction_set_up_command"
+				subgame_set_up_command = "subgame_set_up_command"
 
-# 			end
-# 		end
-# 	end			
+				path_obj.stub(:analysis_script_path).and_return(analysis_script_path)
+				path_obj.stub(:remote_input_path).and_return(remote_input_path)
+				path_obj.stub(:input_file_name).and_return(input_file_name)
+				path_obj.stub(:reduction_script_path).and_return(reduction_script_path)
+				path_obj.stub(:remote_subgame_path).and_return(remote_subgame_path)
+				path_obj.stub(:subgame_json_file_name).and_return(subgame_json_file_name)
+				path_obj.stub(:subgame_script_path).and_return(subgame_script_path)
+				path_obj.stub(:scripts_path).and_return(scripts_path)
+				path_obj.stub(:working_dir).and_return(work_dir)
+
+				File.stub(:join).with(remote_input_path, input_file_name).and_return("remote_input_path/input_file_name")
+				File.stub(:join).with(remote_subgame_path,subgame_json_file_name).and_return("remote_subgame_path/subgame_json_file_name")
+				analysis_obj.should_receive(:set_up_remote_script).with(analysis_script_path,work_dir).and_return(analysis_set_up_remote_script_command)
+				analysis_obj.should_receive(:set_up_remote_input).with("remote_input_path/input_file_name",work_dir).and_return(analysis_set_up_remote_input_command)
+				reduction_obj.should_receive(:set_up_remote_script).with(reduction_script_path, work_dir).and_return(reduction_set_up_command)
+				subgame_obj.should_receive(:set_up_remote).with("remote_subgame_path/subgame_json_file_name",subgame_script_path, work_dir ).and_return(subgame_set_up_command)
+				expect(@setter.set_up_remote_command).to eq("module load python/2.7.5\n" \
+				   "mkdir $JOB_ID\n" \
+	               "analysis_set_up_remote_script_command\n" \
+	               "analysis_set_up_remote_input_command\n\n" \
+	               "reduction_set_up_command\n" \
+	               "subgame_set_up_command\n" \
+	               "cd $JOB_ID\n" \
+	               "export PYTHONPATH=$PYTHONPATH:scripts_path\n")				
+			end
+
+			it "gets the right paths" do
+				work_dir = "$JOB_ID"
+				remote_input_path = "remote_input_path"
+				input_file_name = "input_file_name"
+				reduction_script_path = "reduction_script_path"
+				remote_subgame_path = "remote_subgame_path"
+				subgame_json_file_name = "subgame_json_file_name"
+				subgame_script_path = "subgame_script_path"
+				analysis_script_path = "analysis_script_path"
+				scripts_path = "scripts_path"
+
+				analysis_set_up_remote_script_command = "analysis_set_up_remote_script_command"
+				analysis_set_up_remote_input_command = "analysis_set_up_remote_input_command"
+				reduction_set_up_command = "reduction_set_up_command"
+				subgame_set_up_command = "subgame_set_up_command"
+
+				path_obj.should_receive(:analysis_script_path).and_return(analysis_script_path)
+				path_obj.should_receive(:remote_input_path).and_return(remote_input_path)
+				path_obj.should_receive(:input_file_name).and_return(input_file_name)
+				path_obj.should_receive(:reduction_script_path).and_return(reduction_script_path)
+				path_obj.should_receive(:remote_subgame_path).and_return(remote_subgame_path)
+				path_obj.should_receive(:subgame_json_file_name).and_return(subgame_json_file_name)
+				path_obj.should_receive(:subgame_script_path).and_return(subgame_script_path)
+				path_obj.should_receive(:scripts_path).and_return(scripts_path)
+				path_obj.should_receive(:working_dir).and_return(work_dir)
+
+				File.should_receive(:join).with(remote_input_path, input_file_name).and_return("remote_input_path/input_file_name")
+				File.should_receive(:join).with(remote_subgame_path,subgame_json_file_name).and_return("remote_subgame_path/subgame_json_file_name")
+				analysis_obj.stub(:set_up_remote_script).with(analysis_script_path,work_dir).and_return(analysis_set_up_remote_script_command)
+				analysis_obj.stub(:set_up_remote_input).with("remote_input_path/input_file_name",work_dir).and_return(analysis_set_up_remote_input_command)
+				reduction_obj.stub(:set_up_remote_script).with(reduction_script_path, work_dir).and_return(reduction_set_up_command)
+				subgame_obj.stub(:set_up_remote).with("remote_subgame_path/subgame_json_file_name",subgame_script_path, work_dir ).and_return(subgame_set_up_command)
+				@setter.set_up_remote_command
+			end
+		end
+
+		describe "#get_script_command" do
+			it "runs three scripts" do
+				input_file_name = "input_file_name"
+				reduction_file_name = "reduction_file_name"
+				subgame_json_file_name = "subgame_json_file_name"
+				output_file_name = "output_file_name"
+				running_reduction_command = "running_reduction_command"
+				running_subgame_command = "running_subgame_command"
+				running_analysis_command = "running_analysis_command"
+
+				path_obj.stub(input_file_name).and_return(input_file_name)
+				path_obj.stub(reduction_file_name).exactly(3).times.and_return(reduction_file_name)
+				path_obj.stub(subgame_json_file_name).exactly(3).times.and_return(subgame_json_file_name)
+				path_obj.stub(output_file_name).and_return(output_file_name)
+				
+				reduction_obj.should_receive(:run_with_option).with(input_file_name,reduction_file_name).and_return(running_reduction_command)
+				subgame_obj.should_receive(:run_with_option).with(reduction_file_name,subgame_json_file_name,subgame_json_file_name).and_return(running_subgame_command)
+				analysis_obj.should_receive(:run_with_option).with(reduction_file_name, output_file_name, "-sg subgame_json_file_name").and_return(running_analysis_command)
+
+				expect(@setter.get_script_command).to eq("running_reduction_command\n" \
+				   "running_subgame_command\n" \
+	               "running_analysis_command\n")
+			end
+
+			it "gets the right paths" do
+				input_file_name = "input_file_name"
+				reduction_file_name = "reduction_file_name"
+				subgame_json_file_name = "subgame_json_file_name"
+				output_file_name = "output_file_name"
+				running_reduction_command = "running_reduction_command"
+				running_subgame_command = "running_subgame_command"
+				running_analysis_command = "running_analysis_command"
+
+				path_obj.should_receive(input_file_name).and_return(input_file_name)
+				path_obj.should_receive(reduction_file_name).exactly(3).times.and_return(reduction_file_name)
+				path_obj.should_receive(subgame_json_file_name).exactly(3).times.and_return(subgame_json_file_name)
+				path_obj.should_receive(output_file_name).and_return(output_file_name)
+				
+				reduction_obj.stub(:run_with_option).with(input_file_name,reduction_file_name).and_return(running_reduction_command)
+				subgame_obj.stub(:run_with_option).with(reduction_file_name,subgame_json_file_name,subgame_json_file_name).and_return(running_subgame_command)
+				analysis_obj.stub(:run_with_option).with(reduction_file_name, output_file_name, "-sg subgame_json_file_name").and_return(running_analysis_command)
+
+				@setter.get_script_command
+			end
+		end
+
+		describe "#clean_up_remote_command" do
+			it "gets the analysis and subgame script outputs back" do
+				work_dir = "$JOB_ID"
+				remote_output_path = "remote_output_path"
+				subgame_json_file_name = "subgame_json_file_name"
+				remote_subgame_path = "remote_subgame_path"
+				output_file_name = "output_file_name"
+				analysis_clean_up = "analysis_clean_up"
+				subgame_clean_up = "subgame_clean_up"
+
+				path_obj.stub(:working_dir).exactly(3).times.and_return(work_dir)
+				path_obj.stub(:output_file_name).and_return(output_file_name)
+				path_obj.stub(:remote_output_path).and_return(remote_output_path)
+				path_obj.stub(:subgame_json_file_name).and_return(subgame_json_file_name)
+				path_obj.stub(:remote_subgame_path).and_return(remote_subgame_path)
+
+				analysis_obj.should_receive(:get_output).with(work_dir, output_file_name, remote_output_path).and_return(analysis_clean_up)
+				subgame_obj.should_receive(:get_output).with(work_dir,subgame_json_file_name, remote_subgame_path).and_return(subgame_clean_up)
+
+				expect(@setter.clean_up_remote_command).to eq("analysis_clean_up\n" \
+				   "subgame_clean_up\n" \
+	               "rm -rf $JOB_ID\n")
+
+			end
+		end
+
+		describe "#clean_up_remote_command" do
+			it "gets the right paths" do
+				work_dir = "$JOB_ID"
+				remote_output_path = "remote_output_path"
+				subgame_json_file_name = "subgame_json_file_name"
+				remote_subgame_path = "remote_subgame_path"
+				output_file_name = "output_file_name"
+				analysis_clean_up = "analysis_clean_up"
+				subgame_clean_up = "subgame_clean_up"
+
+				path_obj.should_receive(:working_dir).exactly(3).times.and_return(work_dir)
+				path_obj.should_receive(:output_file_name).and_return(output_file_name)
+				path_obj.should_receive(:remote_output_path).and_return(remote_output_path)
+				path_obj.should_receive(:subgame_json_file_name).and_return(subgame_json_file_name)
+				path_obj.should_receive(:remote_subgame_path).and_return(remote_subgame_path)
+
+				analysis_obj.stub(:get_output).with(work_dir, output_file_name, remote_output_path).and_return(analysis_clean_up)
+				subgame_obj.stub(:get_output).with(work_dir,subgame_json_file_name, remote_subgame_path).and_return(subgame_clean_up)
+
+				@setter.clean_up_remote_command
+
+			end
+		end
+	end
+	context "when reduction is checked,  subgame option is not" do
+		pending
+	end
+
+	context "when subgame is checked,  reduction option is not" do
+		pending
+	end
+
+	context "when subgame and reduction are both disabled" do
+		pending
+	end
 	
-	
-	
-
-# end
-
-# class ScriptsArgumentSetter
-# 	def initialize( analysis_obj,reduction_obj = nil,subgame_obj = nil)
-# 		@analysis_obj = analysis_obj
-# 		@reduction_obj = reduction_obj
-# 		@subgame_obj = subgame_obj
-# 	end
-
-# 	def set_path(path_obj)
-# 		@path_obj = path_obj
-# 	end
-
-# 	def prepare_input(game)
-# 		@analysis_obj.prepare_input(game, @path_obj.local_input_path, @path_obj.input_file_name)
-# 		if @subgame_obj != nil
-# 			@subgame_obj.prepare_input(game, @path_obj.local_subgame_path, @path_obj.subgame_json_file_name)
-# 		end
-# 	end
-
-# 	def set_up_remote_command
-# 		work_dir = @path_obj.working_dir
-# 		analysis_set_up_command = <<-DOCUMENT
-# #{@analysis_obj.set_up_remote_script(@path_obj.analysis_script_path,work_dir)}
-# #{@analysis_obj.set_up_remote_input(File.join(@path_obj.remote_input_path, @path_obj.input_file_name), work_dir)}
-# 		DOCUMENT
-# 		if @reduction_obj !=nil
-# 			reduction_set_up_command = "#{@reduction_obj.set_up_remote_script(@path_obj.reduction_script_path,work_dir)}"
-# 		end
-# 		if @subgame_obj != nil
-# 			subgame_set_up_command = "#{@subgame_obj.set_up_remote(File.join(@path_obj.remote_subgame_path, @path_obj.subgame_json_file_name),@path_obj.subgame_script_path, work_dir)}"			
-# 		end
-
-# 		<<-DOCUMENT
-# module load python/2.7.5
-# mkdir #{work_dir}
-# #{analysis_set_up_command}
-# #{reduction_set_up_command}
-# #{subgame_set_up_command}
-# cd #{work_dir}
-# export PYTHONPATH=$PYTHONPATH:#{@path_obj.scripts_path}
-# 		DOCUMENT
-# 	end
-	
-# 	def get_script_command
-# 		if @reduction_obj !=nil && @subgame_obj !=nil 
-			
-# 			<<-DOCUMENT
-# #{@reduction_obj.run_with_option(@path_obj.input_file_name, @path_obj.reduction_file_name)}
-# #{@subgame_obj.run_with_option(@path_obj.reduction_file_name, @path_obj.subgame_json_file_name, @path_obj.subgame_json_file_name)}
-# #{@analysis_obj.run_with_option(@path_obj.reduction_file_name, @path_obj.output_file_name,"-sg #{@path_obj.subgame_json_file_name}")}
-# 			DOCUMENT
-		
-# 		elsif @reduction_obj == nil && @subgame_obj != nil 
-
-# 			<<-DOCUMENT
-# #{@subgame_obj.run_with_option(@path_obj.input_file_name, @path_obj.subgame_json_file_name, @path_obj.subgame_json_file_name)}
-# #{@analysis_obj.run_with_option(@path_obj.input_file_name, @path_obj.output_file_name,"-sg #{@path_obj.subgame_json_file_name}")}
-# 			DOCUMENT
-
-# 		elsif @reduction_obj !=nil && @subgame_obj == nil 
-# 			<<-DOCUMENT
-# #{@reduction_obj.run_with_option(@path_obj.input_file_name, @path_obj.reduction_file_name)}
-# #{@analysis_obj.run_with_option(@path_obj.reduction_file_name, @path_obj.output_file_name)}		
-# 			DOCUMENT
-# 		else
-# 			"#{@analysis_obj.run_with_option(@path_obj.input_file_name, @path_obj.output_file_name)}"		
-# 		end
-			
-# 	end
-
-# 	def clean_up_remote_command
-# 		analysis_clean_up = @analysis_obj.get_output(@path_obj.working_dir, @path_obj.output_file_name, @path_obj.remote_output_path)
-		
-# 		if @subgame_obj != nil
-# 			subgame_clean_up = @subgame_obj.get_output(@path_obj.working_dir, @path_obj.subgame_json_file_name, @path_obj.remote_subgame_path)
-# 		end
-
-# 		<<-DOCUMENT
-# #{analysis_clean_up}
-# #{subgame_clean_up}
-# rm -rf #{@path_obj.working_dir}
-# 		DOCUMENT
-
-# 	end
-# end
+end	
