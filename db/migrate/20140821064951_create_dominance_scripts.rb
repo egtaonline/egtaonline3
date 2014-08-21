1c217321679a42ecbc1ed640a1481bd3ae987b77
Yao <liuyaoly@umich.edu>
2014-08-21 15:26:17 -0400

    revised column json format

--- db/migrate/20140821064951_create_dominance_scripts.rb
+++ db/migrate/20140821064951_create_dominance_scripts.rb
@@ -1,7 +1,7 @@
 class CreateDominanceScripts < ActiveRecord::Migration
   def change
     create_table :dominance_scripts do |t|
-      t.text :output
+      t.json :output
       t.integer :analysis_id
 
       t.timestamps
