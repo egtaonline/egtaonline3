4a8c7f80e5510dece8ec918e7c968c111127a57c
Yao <liuyaoly@umich.edu>
2014-09-01 17:46:51 -0400

    data parser revised

---
+++ spec/workers/analysis_checker_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe AnalysisChecker do
+  describe '#perform' do
+    let(:analyses) { [double('analysis1'), double('analysis2')] }
+    let(:updatter) {double ("updatter")}
+    before do
+      Analysis.should_receive(:active).and_return(analyses)
+    end
+
+    it 'updates each active analysis' do
+      
+      AnalysisUpdatter.should_receive(:new).and_return(updatter)
+      updatter.should_receive(:update_analysis).with(analyses)
+      subject.perform
+    end
+  end
+end
