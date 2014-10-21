507958c7a663eb1ac108a2daeae483003bb3e054
achintp <achint.prakash@gmail.com>
2014-10-21 18:32:01 -0400

    Changed queue to backend

--- app/workers/analysis_checker.rb
+++ app/workers/analysis_checker.rb
@@ -1,6 +1,6 @@
 class AnalysisChecker
 	include Sidekiq::Worker
-	sidekiq_options queue: 'high_concurrency'
+	sidekiq_options queue: 'backend'
 	def perform
 	    ActiveRecord::Base.transaction do
 	      AnalysisUpdatter.new.update_analysis(Analysis.active)
