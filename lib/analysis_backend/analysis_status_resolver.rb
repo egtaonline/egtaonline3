06e9b7a5f3ec91fe458051f57c39d6a3a3128072
achintp <achint.prakash@gmail.com>
2014-10-22 14:43:26 -0400

    Analysis status resolver reads from local path

--- lib/analysis_backend/analysis_status_resolver.rb
+++ lib/analysis_backend/analysis_status_resolver.rb
@@ -12,7 +12,7 @@ class AnalysisStatusResolver
 	    when 'C', '', nil
           Rails.logger.info "File path:#{File.exist?(File.join(@path_finder.local_pbs_path, @path_finder.pbs_error_file))}" 
 	      if File.exist?(File.join(@path_finder.local_pbs_path, @path_finder.pbs_error_file))
-	        error_message = File.open(File.join(@path_finder.remote_pbs_path, @path_finder.pbs_error_file))
+	        error_message = File.open(File.join(@path_finder.local_pbs_path, @path_finder.pbs_error_file))
 	          .read(ERROR_LIMIT)
 	        if error_message
 	          @analysis.fail(error_message)
