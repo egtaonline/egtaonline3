b2941f513b7488ac341aa5eb0b81b0a891a9537c
Achintya Prakash <achintya@umich.edu>
2015-03-11 22:17:45 -0400

    Remove the usage of nyx
    Clean up if nothing breaks

--- lib/backend/flux/flux_policy.rb
+++ lib/backend/flux/flux_policy.rb
@@ -4,13 +4,14 @@ class FluxPolicy
   end
 
   def assign_queue(simulation)
-    cac_count = Simulation.active_on_other.count
-    flux_available = Simulation.active_on_flux.count - @flux_active_limit
-    if  4 * cac_count > (flux_available) || cac_count > 100
-      simulation.update_attributes(qos: 'flux')
-    else
-      simulation.update_attributes(qos: 'engin_flux')
-    end
+    # cac_count = Simulation.active_on_other.count
+    # flux_available = Simulation.active_on_flux.count - @flux_active_limit
+    # if  4 * cac_count > (flux_available) || cac_count > 100
+    #   simulation.update_attributes(qos: 'flux')
+    # else
+    #   simulation.update_attributes(qos: 'engin_flux')
+    # end
+    simulation.update_attributes(qos: 'flux')
     simulation
   end
 end
